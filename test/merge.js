71661c0df302c5b56b1297f94c472cbb58fbbe6f
Beatriz de Miguel <beatriz.demiguel@bbva.com>
2018-01-04 11:34:30 +0100

    [cross][NO_ISSUE][fix] fix merge files issue

--- test/merge.js
+++ test/merge.js
@@ -7,25 +7,35 @@ import mergeCommand from '../src/commands/merge';
 describe('mergeCommand', () => {
 	context('given three sample files', () => {
 		beforeEach(() => {
+			const sample1 = {
+				foo: {
+					bar: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia dolor sed blandit aliquam.'
+				},
+				foz: {
+					baz: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia dolor sed blandit aliquam.'
+				}
+			};
+			const sample2 = {
+				foo: {
+					bar: 'Lorem dolor dolor dolor dolor, consectetur adipiscing elit. Sed lacinia dolor sed blandit aliquam.'
+				}
+			};
+			const sample3 = {
+				foz: {
+					baw: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia dolor sed blandit aliquam.'
+				}
+			};
+			const sample4 = {
+				foz: {
+					foo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia dolor sed blandit aliquam.'
+				}
+			};
+
 			fs({
-				'sample_1.json': JSON.stringify({
-					foo: {
-						bar: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia dolor sed blandit aliquam.'
-					},
-					foz: {
-						baz: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia dolor sed blandit aliquam.'
-					}
-				}),
-				'sample_2.json': JSON.stringify({
-					foo: {
-						bar: 'Lorem dolor dolor dolor dolor, consectetur adipiscing elit. Sed lacinia dolor sed blandit aliquam.'
-					}
-				}),
-				'sample_3.json': JSON.stringify({
-					foz: {
-						baw: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia dolor sed blandit aliquam.'
-					}
-				})
+				'sample_1.json': JSON.stringify(sample1),
+				'sample_2.json': JSON.stringify(sample2),
+				'sample_3.json': JSON.stringify(sample3),
+				'sample_4.json': JSON.stringify(sample4)
 			});
 		});
 
@@ -40,11 +50,12 @@ describe('mergeCommand', () => {
 				},
 				foz: {
 					baw: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia dolor sed blandit aliquam.',
-					baz: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia dolor sed blandit aliquam.'
+					baz: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia dolor sed blandit aliquam.',
+					foo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia dolor sed blandit aliquam.'
 				}
 			};
 
-			yield mergeCommand('sample_1.json', ['sample_2.json', 'sample_3.json'], yield);
+			yield mergeCommand('sample_1.json', ['sample_2.json', 'sample_3.json', 'sample_4.json'], yield);
 
 			const output = yield loadFile('sample_1.json');
 
